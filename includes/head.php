b68c5a2357405c96f74f926ccdff1d6694a343dc
Erick Sahara <erickmsk8@hotmail.com>
2015-03-22 18:20:45 -0300

    Initial commit

---
+++ includes/head.php
@@ -0,0 +1,15 @@
+<!-- Example of include -->
+<head>
+    <meta charset="utf-8">
+    <title>title</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=1" /> 
+    <!-- home screen iphone/ipad -->
+    <link rel='shortcut icon' type='image/x-icon' href=""/>
+    <link rel="apple-touch-icon" href="">
+    <link rel="apple-touch-icon" sizes="76x76" href="">
+    <link rel="apple-touch-icon" sizes="120x120" href="">
+    <link rel="apple-touch-icon" sizes="152x152" href="">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
